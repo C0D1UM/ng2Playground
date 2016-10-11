31f82c6b85bd90093ff30ddbf048740892ea48d0
Pawarit Saengpreecharutn <pawarit.s@codium.co>
2016-10-11 16:04:40 +0700

    Add one more datetimepicker library for testing with Angular2

--- app/datetimepicker/datetimepicker.component.ts
+++ app/datetimepicker/datetimepicker.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
+import * as moment from 'moment';
 import 'flatpickr';
-//declare var $: JQueryStatic;
+import 'eonasdan-bootstrap-datetimepicker';
+import 'bootstrap-daterangepicker';
 
 @Component({
   moduleId: module.id,
@@ -11,12 +13,65 @@ import 'flatpickr';
 export class datetimePickerComponent implements OnInit {
 
   ngOnInit() {
-    $(".datetimepicker").flatpickr({
-      //"mode": "single",
-      //"time_24hr": true,
-      //"enableSeconds": true,
-      //"enableTime": true
+    let startDatetimePicker, endDatetimepicker;
+    startDatetimePicker = $(".datetimepicker#startDate").flatpickr({
+      "mode": "single",
+      "time_24hr": true,
+      "enableSeconds": true,
+      "enableTime": true,
+      onChange: function (dateObj, dateStr, instance) {
+        let minMoment = moment(dateStr, 'YYYY-MM-DD HH:mm:ss');
+        console.debug('minMoment', minMoment);
+        endDatetimepicker.set('minDate', dateStr);
+      }
     });
+    endDatetimepicker = $(".datetimepicker#endDate").flatpickr({
+      "mode": "single",
+      "time_24hr": true,
+      "enableSeconds": true,
+      "enableTime": true,
+    });
+
+
+    $('#datetimepicker2').datetimepicker({
+      icons: {
+        time: 'fa fa-clock-o',
+        date: 'fa fa-calendar-o',
+        up: 'fa fa-chevron-up',
+        down: 'fa fa-chevron-down',
+        previous: 'fa fa-chevron-left',
+        next: 'fa fa-chevron-right',
+        clear: 'fa fa-trash'
+      }
+    });
+
+    $('#daterangepicker1').daterangepicker({
+      autoApply: true,
+    });
+    $('#daterangepicker2').daterangepicker({
+      autoUpdateInput: true,
+      timePicker: true,
+      timePicker24Hour: true,
+      timePickerIncrement: 30,
+      timePickerSeconds: true,
+      locale: {
+        format: 'DD MMMM YYYY HH:mm:ss'
+      }
+    });
+    $('#daterangepicker3').daterangepicker({
+      autoApply: true,
+      singleDatePicker: true,
+      timePicker: true,
+      timePicker24Hour: true,
+      timePickerIncrement: 1,
+      timePickerSeconds: true,
+      minDate: moment(),
+      locale: {
+        format: 'DD MMMM YYYY HH:mm:ss'
+      }
+    });
+
+
   }
 
 }
--- systemjs.config.js
+++ systemjs.config.js
@@ -3,6 +3,10 @@
  * Adjust as necessary for your application needs.
  */
 (function (global) {
+  System.import('node_modules/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css');
+  System.import('node_modules/font-awesome/css/font-awesome.min.css');
+  System.import('node_modules/bootstrap-daterangepicker/daterangepicker.css');
+
   System.config({
     paths: {
       // paths serve as alias
@@ -21,11 +25,18 @@
       '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+      // SystemJS loader
+      css: 'node_modules/systemjs-plugin-css/css.js',
       // other libraries
       'rxjs': 'npm:rxjs',
+      'lodash': 'npm:lodash',
+      'moment': 'npm:moment',
       'jquery': 'npm:jquery',
       'bootstrap': 'npm:bootstrap',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
+      'eonasdan-bootstrap-datetimepicker': 'npm:eonasdan-bootstrap-datetimepicker',
+      'bootstrap-daterangepicker': 'npm:bootstrap-daterangepicker',
+      'font-awesome': 'npm:font-awesome',
       'select2': 'npm:select2',
       'ng2-select2': 'npm:ng2-select2',
       'ng2-uploader': 'npm:ng2-uploader',
@@ -35,6 +46,14 @@
     packages: {
       app: {
         main: './main.js',
+        defaultExtension: 'js'
+      },
+      lodash: {
+        main: './lodash',
+        defaultExtension: 'min.js'
+      },
+      moment: {
+        main: './moment.js',
         defaultExtension: 'js'
       },
       jquery: {
@@ -51,6 +70,25 @@
       'angular-in-memory-web-api': {
         main: './index.js',
         defaultExtension: 'js'
+      },
+      'eonasdan-bootstrap-datetimepicker': {
+        main: './build/js/bootstrap-datetimepicker',
+        defaultExtension: 'min.js',
+        meta: {
+          'build/css/*.css': { loader: 'css' }
+        }
+      },
+      'bootstrap-daterangepicker': {
+        main: './daterangepicker',
+        defaultExtension: 'js',
+        meta: {
+          '*.css': { loader: 'css' }
+        }
+      },
+      'font-awesome': {
+        meta: {
+          'css/*.css': { loader: 'css' }
+        }
       },
       'select2': {
         defaultExtension: 'min.js',
