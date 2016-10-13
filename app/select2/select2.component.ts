832fb508b34550042f37584a2029ab6c936f9ebf
Pawarit Saengpreecharutn <pawarit.s@codium.co>
2016-10-13 14:43:24 +0700

    Remove all files

--- app/select2/select2.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  moduleId: module.id,
-  selector: 'select-two',
-  templateUrl: 'select2.component.html'
-})
-export class SelectTwoComponent { }
